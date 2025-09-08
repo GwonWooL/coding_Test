71098cb2697ad04baa9d96b830d67dfefc04c86e
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2025-09-08 22:52:41 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 9580 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -4,7 +4,8 @@ let n = Number(input[0])
 let arr = input[1].split("")
 
 let alpha = [0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+let m = 1234567891
+let r = 31
 
 for(let i = 0; i<n; i++) {
     for(let j = 1; j<27; j++) {
@@ -16,7 +17,9 @@ for(let i = 0; i<n; i++) {
 }
 
 let result = 0
-for(let i = 0; i<n; i++) {
-    result += arr[i]*31**i
+let power = 1   // r^i 값을 직접 관리
+for (let i = 0; i < n; i++) {
+    result = (result + arr[i] * power) % m
+    power = (power * r) % m   // r^i -> r^(i+1) 로 갱신
 }
 console.log(result)
