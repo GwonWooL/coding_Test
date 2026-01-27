7f964206879e19ab2071f4048956427ab2651c3b
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2026-01-27 11:44:11 +0900

    [level 2] Title: 괄호 회전하기, Time: 43.39 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,28 +1,32 @@
-function isValid(s) {
-  let check = [];
-  for (let i = 0; i < s.length; i++) {
-    check.push(s[i]);
-
-    let num = check.length;
-
-    if (check[num - 2] === '[' && check[num - 1] === ']')
-      check.splice(num - 2, 2);
-    if (check[num - 2] === '{' && check[num - 1] === '}')
-      check.splice(num - 2, 2);
-    if (check[num - 2] === '(' && check[num - 1] === ')')
-      check.splice(num - 2, 2);
+function isValid(str) {
+  const stack = [];
+  const object = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  };
+  for (let i = 0; i < str.length; i++) {
+    const chr = str[i];
+    if (object[chr]) {
+      stack.push(chr);
+    } else {
+      if (stack.length === 0 || object[stack.pop()] !== chr) {
+        return false;
+      }
+    }
   }
-  if (check.length == 0) {
-    return true;
-  }
+  return stack.length === 0 ? true : false;
 }
 
 function solution(s) {
   let count = 0;
+  // s 길이만큼 for문 반복
   for (let i = 0; i < s.length; i++) {
+    // 문자열이 "올바른 괄호 문자열"인지 확인
     if (isValid(s)) {
       count++;
     }
+    // s문자열 맨 앞 문자를 s문자열의 맨 뒤로 이동
     s = s.slice(1) + s[0];
   }
   return count;
